32979ede4f72dd69b14d581583b14b7dce0cfe77
Rasmus Rudling <rasmusrudling@gmail.com>
2021-03-18 14:23:58 +0100

    Added new social image

---
+++ src/components/pages/PortfolioPage/TechnologiesBarChart/TechnologiesBarChart.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './TechnologiesBarChart .module.scss';
+
+interface Props {
+
+}
+
+const TechnologiesBarChart: React.FC<Props> = () => {
+    return (
+        <div className={classes.TechnologiesBarChart}>
+
+        </div>
+    )
+}
+
+export default TechnologiesBarChart;
