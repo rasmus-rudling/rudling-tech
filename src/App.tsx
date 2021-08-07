3e3237e1f1c83d61cd4ecffa03282d9b8137435f
Rasmus Rudling <rasmusrudling@gmail.com>
2021-08-07 16:51:50 +0200

    Started implementing the option to switch between swedish and english

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { projects } from "./components/pages/PortfolioPage/portfolioUtilities";
+import { svEnProjects } from "./components/pages/PortfolioPage/portfolioUtilities";
 
 import MainMenu from "./components/MainMenu/MainMenu";
 import HomePage from "./components/pages/HomePage/HomePage";
@@ -14,72 +14,72 @@ import IsTouchScreenProvider from "./contexts/IsTouchScreenContext";
 import SelectedProjectProvider from "./contexts/SelectedProjectContext";
 
 const App = () => {
-    const mainMenuButtons = [
-        {
-            pageAddress: "/",
-            text: "Home",
-        },
-        // {
-        //     pageAddress: "blog",
-        //     text: "Blog"
-        // },
-        {
-            pageAddress: "/portfolio",
-            text: "Portfolio",
-        },
-        // {
-        //     pageAddress: "tutorials",
-        //     text: "Tutorials"
-        // }
-    ];
+	const mainMenuButtons = [
+		{
+			pageAddress: "/",
+			text: "Home",
+		},
+		// {
+		//     pageAddress: "blog",
+		//     text: "Blog"
+		// },
+		{
+			pageAddress: "/portfolio",
+			text: "Portfolio",
+		},
+		// {
+		//     pageAddress: "tutorials",
+		//     text: "Tutorials"
+		// }
+	];
 
-    let projectPaths = Object.keys(projects).map((projectName) =>
-        projectName.split(" ").join("_")
-    );
+	let projectPaths = Object.keys(svEnProjects).map((projectName) =>
+		projectName.split(" ").join("_")
+	);
 
-    return (
-        <IsTouchScreenProvider>
-            <SelectedProjectProvider>
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                            <MainMenu menuButtons={mainMenuButtons} />
-                            <HomePage />
-                        </Route>
+	return (
+		<IsTouchScreenProvider>
+			<SelectedProjectProvider>
+				<Router>
+					<Switch>
+						<Route exact path="/">
+							<MainMenu menuButtons={mainMenuButtons} />
+							<HomePage />
+						</Route>
 
-                        <Route exact path="/blog">
-                            <MainMenu menuButtons={mainMenuButtons} />
-                            <HomePage />
-                        </Route>
+						<Route exact path="/blog">
+							<MainMenu menuButtons={mainMenuButtons} />
+							<HomePage />
+						</Route>
 
-                        <Route exact path="/portfolio">
-                            <MainMenu menuButtons={mainMenuButtons} />
-                            <PortfolioPage />
-                        </Route>
+						<Route exact path="/portfolio">
+							<MainMenu menuButtons={mainMenuButtons} />
+							<PortfolioPage />
+						</Route>
 
-                        {projectPaths.map((projectPath) => {
-                            // console.log(`portfolio/details/${projectPath}`);
-                            return (
-                                <Route
-                                    exact
-                                    path={`/portfolio/details/${projectPath}`}
-                                    key={projectPath}
-                                >
-                                    <MainMenu menuButtons={mainMenuButtons} />
-                                    <ProjectDetails />
-                                </Route>
-                            );
-                        })}
+						{projectPaths.map((projectPath) => {
+							// console.log(`portfolio/details/${projectPath}`);
+							return (
+								<Route
+									exact
+									path={`/portfolio/details/${projectPath}`}
+									key={projectPath}
+								>
+									<MainMenu menuButtons={mainMenuButtons} />
+									<ProjectDetails />
+								</Route>
+							);
+						})}
 
-                        <Route exact path="/tutorials">
-                            <MainMenu menuButtons={mainMenuButtons} />
-                            <HomePage />
-                        </Route>
-                    </Switch>
-                </Router>
-            </SelectedProjectProvider>
-        </IsTouchScreenProvider>
-    );
+						<Route exact path="/tutorials">
+							<MainMenu menuButtons={mainMenuButtons} />
+							<HomePage />
+						</Route>
+					</Switch>
+				</Router>
+			</SelectedProjectProvider>
+		</IsTouchScreenProvider>
+	);
 };
 
 export default App;
--- src/components/pages/PortfolioPage/PortfolioPage.tsx
+++ src/components/pages/PortfolioPage/PortfolioPage.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import classes from "./portfolioPage.module.scss";
-import { projects, technologiesInfo, Project } from "./portfolioUtilities";
+import {
+	projects,
+	technologiesInfo,
+	Project,
+	svEnProjects,
+	SvEnProject,
+} from "./portfolioUtilities";
 
 // === COMPONENTS ===
 import ThreeDimButton from "../../common/Buttons/ThreeDimButton/ThreeDimButton";
@@ -13,272 +19,272 @@ import TechnologiesBarChart from "./TechnologiesBarChart/TechnologiesBarChart.js
 // ==================
 
 interface typesDict {
-    [key: string]: any;
+	[key: string]: any;
 }
 
 export interface TechnologiesCounter {
-    [key: string]: number;
+	[key: string]: number;
 }
 
 export interface TechnologiesCounterObject {
-    technology: string;
-    count: number;
+	technology: string;
+	count: number;
 }
 
 // TODO: Make visually appealing summary of the technologies I've been using
 const PortfolioPage: React.FC = () => {
-    const changeProjectType = (newProjectType: string) => {
-        if (selectedProjectType !== newProjectType) {
-            setHideMobileProjectsMenu(true);
-        }
+	const changeProjectType = (newProjectType: string) => {
+		if (selectedProjectType !== newProjectType) {
+			setHideMobileProjectsMenu(true);
+		}
 
-        let tempProjectTypes = [...projectTypes];
+		let tempProjectTypes = [...projectTypes];
 
-        tempProjectTypes.forEach((option) => {
-            option.isSelected = option.type === newProjectType;
-        });
+		tempProjectTypes.forEach((option) => {
+			option.isSelected = option.type === newProjectType;
+		});
 
-        setProjectTypes(tempProjectTypes);
-        setSelectedProjectType(newProjectType);
+		setProjectTypes(tempProjectTypes);
+		setSelectedProjectType(newProjectType);
 
-        let newProjectsToShow = getNewProjectsToShow("", newProjectType, true);
-        setProjectsToShow(newProjectsToShow);
+		let newProjectsToShow = getNewProjectsToShow("", newProjectType, true);
+		setProjectsToShow(newProjectsToShow);
 
-        setTimeout(() => setHideMobileProjectsMenu(false), 200);
-    };
+		setTimeout(() => setHideMobileProjectsMenu(false), 200);
+	};
 
-    const changeCurrentProject = (newCurrentProject: string) => {
-        setProjectsToShow(getNewProjectsToShow(newCurrentProject));
-        setSelectedProject(projects[newCurrentProject]);
-    };
+	const changeCurrentProject = (newCurrentProject: string) => {
+		setProjectsToShow(getNewProjectsToShow(newCurrentProject));
+		setSelectedProject(projects[newCurrentProject]);
+	};
 
-    const getNewProjectsToShow = (
-        newCurrentProject: string,
-        _selectedProjectType = selectedProjectType,
-        setWithIdx = false
-    ) => {
-        let tempProjectsToShow: Array<typesDict> = [],
-            projectIdx = 0;
+	const getNewProjectsToShow = (
+		newCurrentProject: string,
+		_selectedProjectType = selectedProjectType,
+		setWithIdx = false
+	) => {
+		let tempProjectsToShow: Array<typesDict> = [],
+			projectIdx = 0;
 
-        Object.keys(projects).forEach((projectKey) => {
-            let project = projects[projectKey];
-            if (project.type === _selectedProjectType) {
-                let optionObject: typesDict = {};
-                optionObject["type"] = project.name;
-                if (setWithIdx) {
-                    if (projectIdx === 0) {
-                        setSelectedProject(project);
-                        optionObject["isSelected"] = true;
-                    } else {
-                        optionObject["isSelected"] = false;
-                    }
-                } else {
-                    optionObject["isSelected"] =
-                        project.name === newCurrentProject;
-                }
+		Object.keys(projects).forEach((projectKey) => {
+			let project = projects[projectKey];
+			if (project.type === _selectedProjectType) {
+				let optionObject: typesDict = {};
+				optionObject["type"] = project.name;
+				if (setWithIdx) {
+					if (projectIdx === 0) {
+						setSelectedProject(project);
+						optionObject["isSelected"] = true;
+					} else {
+						optionObject["isSelected"] = false;
+					}
+				} else {
+					optionObject["isSelected"] =
+						project.name === newCurrentProject;
+				}
 
-                tempProjectsToShow.push(optionObject);
-                projectIdx++;
-            }
-        });
-        return tempProjectsToShow;
-    };
+				tempProjectsToShow.push(optionObject);
+				projectIdx++;
+			}
+		});
+		return tempProjectsToShow;
+	};
 
-    const [showAllDetails, setShowAllDetails] = useState<boolean>(
-        window.innerWidth < 535
-    );
-    const [projectTypes, setProjectTypes] = useState<Array<typesDict>>([
-        {
-            type: "Personal",
-            isSelected: true,
-        },
-        {
-            type: "Professional",
-            isSelected: false,
-        },
-        {
-            type: "School",
-            isSelected: false,
-        },
-    ]);
-    const [selectedProjectType, setSelectedProjectType] =
-        useState<string>("Personal");
-    const [selectedProject, setSelectedProject] = useState<Project>(
-        projects["Learn AI"]
-    );
-    const [projectsToShow, setProjectsToShow] = useState<Array<typesDict>>(
-        getNewProjectsToShow("Learn AI")
-    );
-    const [hideMobileProjectsMenu, setHideMobileProjectsMenu] =
-        useState<boolean>(false);
+	const [showAllDetails, setShowAllDetails] = useState<boolean>(
+		window.innerWidth < 535
+	);
+	const [projectTypes, setProjectTypes] = useState<Array<typesDict>>([
+		{
+			type: "Personal",
+			isSelected: true,
+		},
+		{
+			type: "Professional",
+			isSelected: false,
+		},
+		{
+			type: "School",
+			isSelected: false,
+		},
+	]);
+	const [selectedProjectType, setSelectedProjectType] =
+		useState<string>("Personal");
+	const [selectedProject, setSelectedProject] = useState<Project>(
+		projects["Learn AI"]
+	);
+	const [projectsToShow, setProjectsToShow] = useState<Array<typesDict>>(
+		getNewProjectsToShow("Learn AI")
+	);
+	const [hideMobileProjectsMenu, setHideMobileProjectsMenu] =
+		useState<boolean>(false);
 
-    window.addEventListener("resize", () => {
-        setShowAllDetails(window.innerWidth < 535);
-    });
+	window.addEventListener("resize", () => {
+		setShowAllDetails(window.innerWidth < 535);
+	});
 
-    const barChartWidth =
-        window.innerWidth > 500 ? 500 : window.innerWidth * 0.8;
-    let technologiesCounter: TechnologiesCounter = {};
+	const barChartWidth =
+		window.innerWidth > 500 ? 500 : window.innerWidth * 0.8;
+	let technologiesCounter: TechnologiesCounter = {};
 
-    Object.keys(projects).forEach((projectName) => {
-        let projectTechnologies = projects[projectName].technologies;
+	Object.keys(projects).forEach((projectName) => {
+		let projectTechnologies = projects[projectName].technologies;
 
-        projectTechnologies.forEach((technology) => {
-            if (technologiesCounter[technology] === undefined) {
-                technologiesCounter[technology] = 1;
-            } else {
-                technologiesCounter[technology] += 1;
-            }
-        });
-    });
+		projectTechnologies.forEach((technology) => {
+			if (technologiesCounter[technology] === undefined) {
+				technologiesCounter[technology] = 1;
+			} else {
+				technologiesCounter[technology] += 1;
+			}
+		});
+	});
 
-    let technologiesCounterArray: Array<TechnologiesCounterObject> = [];
+	let technologiesCounterArray: Array<TechnologiesCounterObject> = [];
 
-    Object.keys(technologiesCounter).forEach((technology) => {
-        technologiesCounterArray.push({
-            technology: technology,
-            count: technologiesCounter[technology],
-        });
-    });
+	Object.keys(technologiesCounter).forEach((technology) => {
+		technologiesCounterArray.push({
+			technology: technology,
+			count: technologiesCounter[technology],
+		});
+	});
 
-    return (
-        <div className={classes.PortfolioPage}>
-            <div className={classes.header}>
-                <h1 style={{ marginBottom: "10px" }}>
-                    Rasmus Rudling Portfolio
-                </h1>
-            </div>
-            <p>
-                On this page you will find a selection of projects that I have
-                been working on during my years as a developer.
-            </p>
+	return (
+		<div className={classes.PortfolioPage}>
+			<div className={classes.header}>
+				<h1 style={{ marginBottom: "10px" }}>
+					Rasmus Rudling Portfolio
+				</h1>
+			</div>
+			<p>
+				On this page you will find a selection of projects that I have
+				been working on during my years as a developer.
+			</p>
 
-            <TechnologiesBarChart
-                technologiesCounter={technologiesCounterArray}
-                width={barChartWidth}
-            />
+			<TechnologiesBarChart
+				technologiesCounter={technologiesCounterArray}
+				width={barChartWidth}
+			/>
 
-            {/* === DESKTOP === */}
-            <div className={classes.projectsContainer}>
-                {Object.keys(projects).map((projectKey) => {
-                    let project = projects[projectKey];
-                    return (
-                        <ProjectCard
-                            projectInfo={project}
-                            showAllDetails={showAllDetails}
-                            technologiesInfo={technologiesInfo}
-                        />
-                    );
-                })}
-            </div>
+			{/* === DESKTOP === */}
+			<div className={classes.projectsContainer}>
+				{Object.keys(projects).map((projectKey) => {
+					let project = projects[projectKey];
+					return (
+						<ProjectCard
+							projectInfo={project}
+							showAllDetails={showAllDetails}
+							technologiesInfo={technologiesInfo}
+						/>
+					);
+				})}
+			</div>
 
-            {/* === MOBILE === */}
-            <div className={classes.mobileProjectsContainer}>
-                <MultipleChoiceButtonPrimary
-                    options={projectTypes}
-                    changeOption={changeProjectType}
-                    extraClass={classes.projectTypes}
-                />
+			{/* === MOBILE === */}
+			<div className={classes.mobileProjectsContainer}>
+				<MultipleChoiceButtonPrimary
+					options={projectTypes}
+					changeOption={changeProjectType}
+					extraClass={classes.projectTypes}
+				/>
 
-                <div
-                    className={
-                        hideMobileProjectsMenu
-                            ? classes.hideMobileProjectsMenu
-                            : classes.mobileProjectsMenu
-                    }
-                >
-                    <MultipleChoiceButtonSecondary
-                        options={projectsToShow}
-                        changeOption={changeCurrentProject}
-                        extraClass={classes.projectTypes}
-                    />
-                </div>
+				<div
+					className={
+						hideMobileProjectsMenu
+							? classes.hideMobileProjectsMenu
+							: classes.mobileProjectsMenu
+					}
+				>
+					<MultipleChoiceButtonSecondary
+						options={projectsToShow}
+						changeOption={changeCurrentProject}
+						extraClass={classes.projectTypes}
+					/>
+				</div>
 
-                <div className={classes.projectInfoContainer}>
-                    <ImageCarousel
-                        images={selectedProject.images}
-                        extraClasses={[classes.imageCarousel]}
-                    />
+				<div className={classes.projectInfoContainer}>
+					<ImageCarousel
+						images={selectedProject.images}
+						extraClasses={[classes.imageCarousel]}
+					/>
 
-                    <div style={{ fontWeight: "bolder" }}>
-                        Technologies that I used:
-                    </div>
+					<div style={{ fontWeight: "bolder" }}>
+						Technologies that I used:
+					</div>
 
-                    <div className={classes.technologiesUsedContainer}>
-                        {selectedProject.technologies.map((technology) => (
-                            <div className={classes.technology}>
-                                <div className={classes.imageContainer}>
-                                    <img
-                                        src={technologiesInfo[technology].icon}
-                                        alt={""}
-                                    />
-                                </div>
+					<div className={classes.technologiesUsedContainer}>
+						{selectedProject.technologies.map((technology) => (
+							<div className={classes.technology}>
+								<div className={classes.imageContainer}>
+									<img
+										src={technologiesInfo[technology].icon}
+										alt={""}
+									/>
+								</div>
 
-                                <div>{technology}</div>
-                            </div>
-                        ))}
-                    </div>
+								<div>{technology}</div>
+							</div>
+						))}
+					</div>
 
-                    <p
-                        className={classes.longText}
-                        dangerouslySetInnerHTML={{
-                            __html: selectedProject.textLong,
-                        }}
-                    />
-                    <ul>
-                        {selectedProject.bullets.map((bullet) => (
-                            <li dangerouslySetInnerHTML={{ __html: bullet }} />
-                        ))}
-                    </ul>
+					<p
+						className={classes.longText}
+						dangerouslySetInnerHTML={{
+							__html: selectedProject.textLong,
+						}}
+					/>
+					<ul>
+						{selectedProject.bullets.map((bullet) => (
+							<li dangerouslySetInnerHTML={{ __html: bullet }} />
+						))}
+					</ul>
 
-                    <div className={classes.blueButtonsContainer}>
-                        {selectedProject.gitHubLink !== undefined ? (
-                            <ThreeDimButton
-                                text="GitHub"
-                                onClickHandler={() =>
-                                    window.open(
-                                        selectedProject.gitHubLink,
-                                        "_blank"
-                                    )
-                                }
-                                extraClasses={[classes.gitHubButton]}
-                            />
-                        ) : null}
+					<div className={classes.blueButtonsContainer}>
+						{selectedProject.gitHubLink !== undefined ? (
+							<ThreeDimButton
+								text="GitHub"
+								onClickHandler={() =>
+									window.open(
+										selectedProject.gitHubLink,
+										"_blank"
+									)
+								}
+								extraClasses={[classes.gitHubButton]}
+							/>
+						) : null}
 
-                        {selectedProject.demoLink !== undefined ? (
-                            <ThreeDimButton
-                                text="Demo"
-                                onClickHandler={() =>
-                                    window.open(
-                                        selectedProject.demoLink,
-                                        "_blank"
-                                    )
-                                }
-                                extraClasses={[classes.demoButton]}
-                            />
-                        ) : null}
-                    </div>
+						{selectedProject.demoLink !== undefined ? (
+							<ThreeDimButton
+								text="Demo"
+								onClickHandler={() =>
+									window.open(
+										selectedProject.demoLink,
+										"_blank"
+									)
+								}
+								extraClasses={[classes.demoButton]}
+							/>
+						) : null}
+					</div>
 
-                    {selectedProject.name === "The Card Game" ? (
-                        <div className={classes.learnMoreButtonContainer}>
-                            <ThreeDimButton
-                                text="Read the thesis"
-                                onClickHandler={() => {
-                                    const link = document.createElement("a");
-                                    link.href =
-                                        process.env.PUBLIC_URL +
-                                        "/documents/bachelors_thesis.pdf";
-                                    link.target = "_blank";
-                                    link.click();
-                                }}
-                                color="gray"
-                            />
-                        </div>
-                    ) : null}
-                </div>
-            </div>
-        </div>
-    );
+					{selectedProject.name === "The Card Game" ? (
+						<div className={classes.learnMoreButtonContainer}>
+							<ThreeDimButton
+								text="Read the thesis"
+								onClickHandler={() => {
+									const link = document.createElement("a");
+									link.href =
+										process.env.PUBLIC_URL +
+										"/documents/bachelors_thesis.pdf";
+									link.target = "_blank";
+									link.click();
+								}}
+								color="gray"
+							/>
+						</div>
+					) : null}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PortfolioPage;
